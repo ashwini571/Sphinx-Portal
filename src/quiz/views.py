a2cf5e9f6b39f85c683d20eaaa124f589cf9447f
unknown <ojha.ashwini.1998@gmail.com>
2019-04-13 16:54:14 +0530

    URL Bug resolved

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -16,9 +16,9 @@ utc = pytz.UTC
 # Create your views here.
 
 
+
 @login_required(login_url='/login')
 def quiz_auth(request, quizid):
-
     item = Quiz.objects.get(quiz_id=quizid)
     item.duration = item.duration
     users = item.users_appeared.filter(pk=request.user.pk)
@@ -27,7 +27,14 @@ def quiz_auth(request, quizid):
     print(today.tzinfo)
     print(start_time)
     print(today)
-    if(today >start_time ):
+    temp = datetime.combine(date.min, today.time()) - datetime.combine(date.min, item.quiz_time.time())
+    if temp < timedelta(minutes=5):
+        print("hola")
+        margin = 1
+    else:
+        margin = 0
+
+    if(today >start_time and margin ):
 
         if users.exists():
             messages.info(request, 'You already appeared in this Quiz')
@@ -49,7 +56,6 @@ def quiz_auth(request, quizid):
         return redirect('dashboard')
 
 
-
 def create_answer_table(quiz_object, question_objects, user_object):
     for question_object in question_objects:
         list_sheet = get_object_or_404(AnswerSheet, contestant=user_object, quiz=quiz_object)
@@ -63,71 +69,75 @@ def create_answer_table(quiz_object, question_objects, user_object):
 # Function for conducting quiz
 @login_required(login_url='/login')
 def conduct_quiz(request, quizid):
+    # this checks is for preventing user to open quiz through URL
+    if request.session['username'] == quizid:
 
-    aspirant = request.user
-    item = get_object_or_404(Quiz, quiz_id=quizid)
-    data = list(Question.objects.filter(quiz=item))
-    random.shuffle(data)
+        aspirant = request.user
+        item = get_object_or_404(Quiz, quiz_id=quizid)
+        data = list(Question.objects.filter(quiz=item))
+        random.shuffle(data)
 
-    list_sheet = get_object_or_404(AnswerSheet, contestant=aspirant, quiz=item)
+        list_sheet = get_object_or_404(AnswerSheet, contestant=aspirant, quiz=item)
 
-    # Question Submission
-    if request.method == 'POST':
-        if list_sheet.is_valid and not list_sheet.is_attempted:
-            question_object = get_object_or_404(Question, id=request.POST.get('question_id'))
-            answer_object = get_object_or_404(Answer, question=question_object, sheet=list_sheet)
+        # Question Submission
+        if request.method == 'POST':
+            if list_sheet.is_valid and not list_sheet.is_attempted:
+                question_object = get_object_or_404(Question, id=request.POST.get('question_id'))
+                answer_object = get_object_or_404(Answer, question=question_object, sheet=list_sheet)
 
-            if not answer_object.is_attempted:
-                # Update values
-                if question_object.is_subjective:
-                    answer_object.subjective_answer = strip_tags(request.POST.get('subjective_answer'))
-                elif question_object.is_single:
-                    answer_object.response_A = request.POST.get('single_answer')
+                if not answer_object.is_attempted:
+                    # Update values
+                    if question_object.is_subjective:
+                        answer_object.subjective_answer = strip_tags(request.POST.get('subjective_answer'))
+                    elif question_object.is_single:
+                        answer_object.response_A = request.POST.get('single_answer')
 
-                # Multi correct
+                    # Multi correct
+                    else:
+                        check_list = request.POST.getlist('multi_answer[]')
+                        print(check_list[0])
+                        length = len(check_list)
+                        if length == 1:
+                            answer_object.response_A = check_list[0]
+                        elif length == 2:
+                            answer_object.response_A = check_list[0]
+                            answer_object.response_B = check_list[1]
+                        elif length == 3:
+                            answer_object.response_A = check_list[0]
+                            answer_object.response_B = check_list[1]
+                            answer_object.response_C = check_list[2]
+                        elif length == 4:
+                            answer_object.response_A = check_list[0]
+                            answer_object.response_B = check_list[1]
+                            answer_object.response_C = check_list[2]
+                            answer_object.response_D = check_list[3]
+
+                    answer_object.response_time = request.POST.get('response_time')
+                    answer_object.is_attempted = True
+                    answer_object.save()
+
                 else:
-                    check_list = request.POST.getlist('multi_answer[]')
-                    print(check_list[0])
-                    length = len(check_list)
-                    if length == 1:
-                        answer_object.response_A = check_list[0]
-                    elif length == 2:
-                        answer_object.response_A = check_list[0]
-                        answer_object.response_B = check_list[1]
-                    elif length == 3:
-                        answer_object.response_A = check_list[0]
-                        answer_object.response_B = check_list[1]
-                        answer_object.response_C = check_list[2]
-                    elif length == 4:
-                        answer_object.response_A = check_list[0]
-                        answer_object.response_B = check_list[1]
-                        answer_object.response_C = check_list[2]
-                        answer_object.response_D = check_list[3]
-
-                answer_object.response_time = request.POST.get('response_time')
-                answer_object.is_attempted = True
-                answer_object.save()
+                    context = {'title': 'Error', 'messages': 'You have already attempted this question'}
+                    return JsonResponse(context)
 
             else:
-                context = {'title': 'Error', 'messages': 'You have already attempted this question'}
-                return JsonResponse(context)
+                messages.info(request, 'You have already attempted this quiz')
+                return redirect('dashboard')
+
+        querys = []
+        for question in data:
+            querys.append(question)
 
         else:
-            messages.info(request, 'You have already attempted this quiz')
-            return redirect('dashboard')
+            try:
+                answers = get_list_or_404(Answer, sheet=list_sheet)
+            except:
+                create_answer_table(item, data, aspirant)
 
-    querys = []
-    for question in data:
-        querys.append(question)
-
+        return render(request, 'conduct_quiz.html', {'title': 'Best of Luck', 'quiz_object': item, 'quiz_data': querys,
+                                                     'user': aspirant})
     else:
-        try:
-            answers = get_list_or_404(Answer, sheet=list_sheet)
-        except:
-            create_answer_table(item, data, aspirant)
-
-    return render(request, 'conduct_quiz.html', {'title': 'Best of Luck', 'quiz_object': item, 'quiz_data': querys,
-                                                 'user': aspirant})
+        return redirect('dashboard')
 
 
 @login_required(login_url='/login')
@@ -176,7 +186,8 @@ def start_quiz(request):
         except Quiz.DoesNotExist:
             messages.info(request, 'Invalid Quiz-id')
             return redirect('admin_dashboard')
-
+    else:
+         return redirect('dashboard')
 
 @login_required(login_url='/login')
 def register_quiz(request, quizid):
@@ -251,7 +262,7 @@ def end_quiz(request, quizid):
     quiz_object = Quiz.objects.get(quiz_id=quizid)
     user = request.user
     sheet = AnswerSheet.objects.get(quiz=quiz_object, contestant=user)
-
+    quiz_object.users_appeared.add(user)
     answer_list = get_list_or_404(Answer, sheet=sheet)
     subjective_counter = 0
 
